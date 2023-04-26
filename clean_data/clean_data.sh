808b8d92b36a69b4ffaf1de5afa93d6201c2ce92
MasserKato <masserhorn.952@gmail.com>
2023-04-26 18:44:41 +0900

    DPがズレてる列があるのでいい感じに修正

--- clean_data/clean_data.sh
+++ clean_data/clean_data.sh
@@ -31,8 +31,24 @@ zcat $DATADIR$FILENAME | \
 awk '{if(substr($0,1,3)=="chr") print substr($0,4); else print $0}' | \
 # 3. 各行の先頭が"#"で始まる行は、4の処理を行わない。
 awk '{if(substr($0,1,1)!="#") print $0; else print $0}' | \
-# 4. 各行の10列目の文字列について、コロンを区切り文字として３番目に当たる文字列が10未満の数字の場合その行を削除する。
-awk -F '\t' '{if ($0 ~ /^#/) {print} else {split($10, a, ":"); if(a[3]>=10) print}}' | \
+# 4. 各行の10列目の文字列について、コロンを区切り文字としてDPに当たる文字列が10未満の数字の場合その行を削除する。
+awk -F '\t' '{
+    if ($0 ~ /^#/) {
+        print
+    } else {
+        split($9, col9_arr, ":");
+        for (i in col9_arr) {
+            if (col9_arr[i] == "DP") {
+                dp_index = i;
+                break;
+            }
+        }
+        split($10, col10_arr, ":");
+        if (col10_arr[dp_index] >= 10) {
+            print
+        }
+    }
+}' | \
 # 5. 以上の処理を経たデータを、gz圧縮して、OUTPUTDIRディレクトリに保存する
 gzip > $OUTPUTDIR$FILENAME
 
