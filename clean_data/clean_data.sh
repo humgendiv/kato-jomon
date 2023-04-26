b0ef87b5b2b78858c610a4a93cf511f8e7c5a8f7
MasserKato <masserhorn.952@gmail.com>
2023-04-26 17:49:13 +0900

    バグ修正

--- clean_data/clean_data.sh
+++ clean_data/clean_data.sh
@@ -5,14 +5,18 @@
 #$ -l short
 #$ -l d_rt=01:00:00
 #$ -l s_rt=01:00:00
-#$ -l s_vmem=32G
-#$ -l mem_req=32G
+#$ -l s_vmem=16G
+#$ -l mem_req=16G
 #$ -N clean_data
 #$ -S /bin/bash
 
 # vcf.gzファイルのいらない行を削除していく。formatも整える。
 
 DATADIR=/home/mkato952/data/raw_vcf/
+# DATADIRが存在しない場合は作成する
+if [ ! -d $DATADIR ]; then
+    mkdir -p $DATADIR
+fi
 OUTPUTDIR=/home/mkato952/data/clean_vcf/
 # OUTPUTDIRが存在しない場合は作成する
 if [ ! -d $OUTPUTDIR ]; then
@@ -22,13 +26,13 @@ fi
 FILENAME=$1
 
 # 1. gzファイルを展開し、パイプを繋いで１行ずつ処理する
-gzcat $DATADIR$FILENAME | \
-# 2. 各行の先頭の文字が#で始まる行は何もしない
-awk '{if(substr($0,1,1)!="#") print $0}' | \
-# 3. 各行の先頭が"chr"で始まる行は、その先頭のchrを削除する
+zcat $DATADIR$FILENAME | \
+# 2. 各行の先頭が"chr"で始まる行は、その先頭のchrを削除する
 awk '{if(substr($0,1,3)=="chr") print substr($0,4); else print $0}' | \
-# 4. 各行の10列目の文字列のうち、":"で区切ったとき３番目に当たる数字について10未満であればその行を削除する
-awk '{split($10,a,":"); if(a[3]<10) print $0}' | \
+# 3. 各行の先頭が"#"で始まる行は、4の処理を行わない。
+awk '{if(substr($0,1,1)!="#") print $0; else print $0}' | \
+# 4. 各行の10列目の文字列について、コロンを区切り文字として３番目に当たる文字列が10未満の数字の場合その行を削除する。
+awk -F '\t' '{split($10, a, ":"); if(a[3]<10) print $0; else print $0}' | \
 # 5. 以上の処理を経たデータを、gz圧縮して、OUTPUTDIRディレクトリに保存する
-gzip > $OUTPUTDIR$FILENAME.gz
+gzip > $OUTPUTDIR$FILENAME
 
