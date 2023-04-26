535f80689f7462fb339ab9e14050909369aa60eb
MasserKato <masserhorn.952@gmail.com>
2023-04-26 17:24:44 +0900

    スパコン用に細かく調整

--- clean_data/clean_data.sh
+++ clean_data/clean_data.sh
@@ -1,6 +1,24 @@
+#!/bin/bash
+
+#$ -cwd
+#$ -V
+#$ -l short
+#$ -l d_rt=01:00:00
+#$ -l s_rt=01:00:00
+#$ -l s_vmem=32G
+#$ -l mem_req=32G
+#$ -N clean_data
+#$ -S /bin/bash
+
 # vcf.gzファイルのいらない行を削除していく。formatも整える。
 
 DATADIR=/home/mkato952/data/raw_vcf/
+OUTPUTDIR=/home/mkato952/data/clean_vcf/
+# OUTPUTDIRが存在しない場合は作成する
+if [ ! -d $OUTPUTDIR ]; then
+    mkdir -p $OUTPUTDIR
+fi
+
 FILENAME=$1
 
 # 1. gzファイルを展開し、パイプを繋いで１行ずつ処理する
@@ -11,6 +29,6 @@ awk '{if(substr($0,1,1)!="#") print $0}' | \
 awk '{if(substr($0,1,3)=="chr") print substr($0,4); else print $0}' | \
 # 4. 各行の10列目の文字列のうち、":"で区切ったとき３番目に当たる数字について10未満であればその行を削除する
 awk '{split($10,a,":"); if(a[3]<10) print $0}' | \
-# 5. 以上の処理を経たデータを、gz圧縮して、DATADIRディレクトリに保存する
-gzip > $DATADIR$FILENAME".clean.gz"
+# 5. 以上の処理を経たデータを、gz圧縮して、OUTPUTDIRディレクトリに保存する
+gzip > $OUTPUTDIR$FILENAME.gz
 
