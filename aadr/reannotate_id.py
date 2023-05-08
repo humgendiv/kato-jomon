3a61eccecf6971633e98823d0f7ea0e87b651686
MasserKato <mkato@dhcp2-142.biol.s.u-tokyo.ac.jp>
2023-05-08 12:47:41 +0900

    geno file format changed

---
+++ aadr/reannotate_id.py
@@ -0,0 +1,12 @@
+input_file_path = "/Users/mkato/research_icloud/v54.1.p1_1240K_public.snp"
+output_file_path = "/Users/mkato/research_icloud/v54.1.p1_1240K_public_new.snp"
+
+with open(input_file_path, 'r') as input_file, open(output_file_path, 'w') as output_file:
+    for line in input_file:
+        columns = line.strip().split()
+
+        if len(columns) >= 6:
+            columns[0] = f'{columns[1]}:{columns[3]}'
+
+        new_line = '\t'.join(columns)
+        output_file.write(new_line + '\n')
